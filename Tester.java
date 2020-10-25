719f309cbc1987603cf55f2e24ccca0ff9cffcbd
wlin10 <wlin10@stuy.edu>
2020-10-25 15:22:13 -0400

    first sum

---
+++ Tester.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+public class Tester{
+  public static void main(String[]args){
+    int[][]  A = { {  1,  0, 12, -1 },
+                  {  7, -2,  2,  1 },
+                  { -5, -2,  2, -9 }
+               };
+    int[]  B  =   {  1, 3, 5 };
+    
+    System.out.println( ArrayOps.sum(B));
+
+  }
+}

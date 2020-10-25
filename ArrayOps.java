9684d24204c4211117390a57ce2590b64c335b93
wlin10 <wlin10@stuy.edu>
2020-10-25 16:30:00 -0400

    5a

--- ArrayOps.java
+++ ArrayOps.java
@@ -43,4 +43,14 @@ public class ArrayOps{
     return ans;
   }
 
+  public static int[] sumCols (int[][] matrix) {
+    int[] ans = new int[matrix[0].length];
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[0].length; j++) {
+        ans[j] += matrix[i][j];
+      }
+    }
+    return ans;
+  }
+
 }

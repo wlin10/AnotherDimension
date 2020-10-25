8d53baee65e844cd7c70e98c12fa1d9a555362b7
wlin10 <wlin10@stuy.edu>
2020-10-25 18:15:14 -0400

    added isLocationMagic

--- Tester.java
+++ Tester.java
@@ -19,18 +19,26 @@ public class Tester{
 
     //Part 4
     System.out.println(ArrayOps.sum(B));
+
     System.out.println( Arrays.toString(ArrayOps.sumRows(A)));
+
     System.out.println(Arrays.toString(ArrayOps.largestInRows(A)));
+    
     System.out.println(ArrayOps.sum(A));
 
     //Part 5
     System.out.println( Arrays.toString(ArrayOps.sumCols(A)));
+
     System.out.println(ArrayOps.isRowMagic(X));
     System.out.println(ArrayOps.isRowMagic(Y));
     System.out.println(ArrayOps.isRowMagic(Z));
+
     System.out.println(ArrayOps.isColMagic(X));
     System.out.println(ArrayOps.isColMagic(Y));
     System.out.println(ArrayOps.isColMagic(Z));
+
+    System.out.println(ArrayOps.isLocationMagic(E, 0, 1));
+    System.out.println(ArrayOps.isLocationMagic(E, 1, 1));
 
   }
 }

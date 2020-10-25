e28ab72709bb7a273fe8b58ea5c1e42480365d6d
wlin10 <wlin10@stuy.edu>
2020-10-25 17:58:33 -0400

    isRowMagic

--- ArrayOps.java
+++ ArrayOps.java
@@ -53,4 +53,14 @@ public class ArrayOps{
     return ans;
   }
 
+  public static boolean isColMagic(int[][] matrix) {
+    int[] arr = sumCols(matrix);
+    for (int i = 0; i < arr.length - 1; i++) {
+      if (arr[i] != arr[i+1]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
 }

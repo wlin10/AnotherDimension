228aa573c295e70d5ccecbfd984391c29d225383
wlin10 <wlin10@stuy.edu>
2020-10-25 15:14:53 -0400

    fixed 4b

--- ArrayOps.java
+++ ArrayOps.java
@@ -2,7 +2,7 @@ public class ArrayOps{
 
   public static int sum(int[] arr) {
     int ans = 0;
-    for (int i = 0; i <= arr.length; i++) {
+    for (int i = 0; i < arr.length; i++) {
       ans += arr[i];
     }
     return ans;
@@ -10,7 +10,7 @@ public class ArrayOps{
 
   public static int largest(int[] arr) {
     int ans = arr[0];
-    for (int i = 1; i <= arr.length; i++) {
+    for (int i = 1; i < arr.length; i++) {
       if (arr[i] > ans) {
         ans = arr[i];
       }
